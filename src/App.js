5986eb860e23118e2f150c2180f7fa515c30c0b9
Elahesmailpour <eli_es87@yahoo.com>
2021-03-29 14:19:50 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+
 
 function App() {
 
-  const [teilnehmer, setTeilnehmer] = useState([]);
-
-  useEffect(() => {   
-    if (teilnehmer.length===0) {
-      fetch("/teilnehmer").then( (httpResponse) => {
-        // Backend bauen....
-        httpResponse.json().then( antwortObjekt => {
-          const leute = antwortObjekt;
-          setTeilnehmer(leute);
-        })
-      }).catch( fehler => { console.error(fehler)});
-    }
-  });
+ 
 
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Ich bin mein tolles Frontend. geliefert von einem Express-Server.
-          Wurde aktualisiert.
+          Ich bin mein tolles Frontend.
         </p>
-        <ul>
-          {teilnehmer.map( (einTeilnehmer) => {
-            return <li>{einTeilnehmer}</li>
-          })}
-        </ul>
+        
       </header>
     </div>
   );
